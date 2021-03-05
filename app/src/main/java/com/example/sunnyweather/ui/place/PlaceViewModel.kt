95814339e31e026a49933de7ade4f11ca7c48502
xiaoming <1097735900@qq.com>
2021-03-05 11:03:14 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceViewModel.kt
@@ -13,4 +13,7 @@ class PlaceViewModel: ViewModel() {
     fun searchPlaces(query: String){
         searchLiveData.value = query
     }
+    fun savePlace(place: Place) = Repository.savePlace(place)
+    fun getSavedPlace() = Repository.getSavedPlace()
+    fun isPlaceSaved() = Repository.isPlaceSaved()
 }
